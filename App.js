1ea21379ac36f9b1f676d958481e9b92cfd709ed
Malikbasha <148067076+Malikbasha4@users.noreply.github.com>
2024-01-20 22:49:26 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Profile from './components/Profile';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;

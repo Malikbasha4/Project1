d818eb86be402164bea792fa9587b35b9ea85516
Malikbasha <148067076+Malikbasha4@users.noreply.github.com>
2024-01-20 22:51:29 +0530

    Create Profile.js

---
+++ Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Profile</h2>
+      {/* Your profile information and update form here */}
+    </div>
+  );
+};
+
+export default Profile;

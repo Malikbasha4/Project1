b629b9b9896d03614e0adc37f01db2aefd2f4c48
Malikbasha <148067076+Malikbasha4@users.noreply.github.com>
2024-01-20 22:50:13 +0530

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      {/* Your login form and logic here */}
+    </div>
+  );
+};
+
+export default Login;

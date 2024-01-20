23a696552c530afdedd7a5f0cb2adf69fb0e0266
Malikbasha <148067076+Malikbasha4@users.noreply.github.com>
2024-01-20 22:50:56 +0530

    Create Signup.js

---
+++ Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      {/* Your signup form and logic here */}
+    </div>
+  );
+};
+
+export default Signup;
